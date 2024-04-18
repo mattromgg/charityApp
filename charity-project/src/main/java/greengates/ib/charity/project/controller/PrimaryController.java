fb864e5d7ad52b0e1f3eb290748fae4e4b423866
mattromgg <matthew_romero@greengates.edu.mx>
2024-04-18 11:12:45 -0500

    implement basic GUI

--- charity-project/src/main/java/greengates/ib/charity/project/controller/PrimaryController.java
+++ charity-project/src/main/java/greengates/ib/charity/project/controller/PrimaryController.java
@@ -2,8 +2,17 @@ package greengates.ib.charity.project.controller;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
 
 public class PrimaryController {
 
-    
+    @FXML
+    private ChoiceBox<String> selectMonth;
+
+    public void initialize() {
+        String[] months = {"January", "February", "March", "April", "May", "June", "July", "September", "October", "November", "December"}; 
+
+        selectMonth.getItems().addAll(months);
+    }
+
 }
--- charity-project/src/main/java/module-info.java
+++ charity-project/src/main/java/module-info.java
@@ -2,7 +2,7 @@ module greengates.ib.charity.project {
     requires javafx.controls;
     requires javafx.fxml;
 
-    opens greengates.ib.charity.project to javafx.fxml;
+    opens greengates.ib.charity.project.controller to javafx.fxml;
     exports greengates.ib.charity.project;
     exports greengates.ib.charity.project.controller;
 
